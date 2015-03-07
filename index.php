62345bcd69ea6ab8dcb778b4e0de755ce4490d1f
tvart <vartan.torossian@gmail.com>
2015-03-08 00:40:38 +0100

    change name

--- index.php
+++ index.php
@@ -3,9 +3,10 @@ require 'vendor/autoload.php';
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
 
+
 use \libs\Database\Connection as Connection;
 
-echo "hi johny!";
+echo "hi test!";
 /*use \libs\Migration\Migrate;
 use \libs\Migration\Seed;
 
